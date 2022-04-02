76b61ca336a67b3ecdde6b505b94bfc0ca5df47b
ixap2i <ayamashita124@gmail.com>
2022-04-02 23:24:11 +0000

    init for main

---
+++ record-memo/www/public/882.bootstrap.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwww=self.webpackChunkwww||[]).push([[882],{882:(e,t,n)=>{n.r(t);var r=n(294);n(935).render(r.createElement((function(){return r.createElement("div",null,"wasm-react-test")}),null),document.getElementById("root"))}}]);
---
+++ record-memo/www/src/index.tsx
@@ -0,0 +1,6 @@
+import * as wasm from "record-memo";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
