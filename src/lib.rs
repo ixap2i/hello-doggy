57c9e21f4ee1c7a79304453e8220ef49036e8444
ixap2i <ayamashita124@gmail.com>
2022-04-10 22:48:04 +0900

    return dog  obj at rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 mod utils;
-
 use wasm_bindgen::prelude::*;
+use serde::{Deserialize, Serialize};
+use wasm_bindgen::JsCast;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{Request, RequestInit, RequestMode, Response};
+use std::fmt::{self, Debug};
+use std::error;
+use std::error::Error;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -16,4 +22,48 @@ extern {
 #[wasm_bindgen]
 pub fn greet() {
     alert("Hello, record-memo!");
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Dog {
+    pub message: String,
+    pub status: String,
+}
+
+impl fmt::Display for Dog {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "json response has error")
+    }
+}
+
+impl error::Error for Dog {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        None
+    }
+}
+
+#[wasm_bindgen]
+pub async fn run() -> Result<JsValue, JsValue> {
+    let mut opts = RequestInit::new();
+    opts.method("GET");
+    opts.mode(RequestMode::Cors);
+
+    let url = "https://dog.ceo/api/breeds/image/random";
+
+    let request = Request::new_with_str_and_init(url, &opts)?;
+
+    let window = web_sys::window().unwrap();
+    let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
+
+    // `resp_value` is a `Response` object.
+    assert!(resp_value.is_instance_of::<Response>());
+    let resp: Response = resp_value.dyn_into().unwrap();
+
+    // Convert this other `Promise` into a rust `Future`.
+    let json = JsFuture::from(resp.json()?).await?;
+
+    // Use serde to parse the JSON into a struct.
+    let branch_info: Dog = json.into_serde().unwrap();
+
+    Ok(JsValue::from_serde(&branch_info).unwrap())
 }
