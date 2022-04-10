00530e76d827fab5f9b85a18d7f06ae35e49f663
ixap2i <ayamashita124@gmail.com>
2022-04-10 22:48:41 +0900

    experiment ts

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import * as wasm from "record-memo";
+// import { useEffect, useState } from 'react';
+
+var testarr = new Array();
+async function test(): Promise<String> {
+  var test2 = await wasm.run();
+  console.log(test2.message);
+  const str = test2.message;
+  testarr.push(str);
+  return str;
+}
+const t = test().then(() => console.log(testarr));
+const t2 = test().then(() => { return testarr[0] });
 
 const App = () => {
   const test = wasm.greet();
-  const test2 = wasm.db_conn();
+  console.log(t2)
   return (
     <div>
-        <div>wasm-react-test</div>
-        <div>${test2}</div>
+      <div>wasm-react-test</div>
     </div>
   );
 };
