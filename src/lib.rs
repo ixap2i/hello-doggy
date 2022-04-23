50c36f92c0c48267789560d2d4a7f526af20d138
ixap2i <ayamashita124@gmail.com>
2022-04-23 12:13:23 +0900

    display dog image

--- src/lib.rs
+++ src/lib.rs
@@ -42,6 +42,24 @@ impl error::Error for Dog {
     }
 }
 
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PsqlArr {
+    pub data: JsonValue::Array
+}
+
+impl fmt::Display for PsqlArr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "json response has error")
+    }
+}
+
+impl error::Error for PsqlArr {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        None
+    }
+}
+
 #[wasm_bindgen]
 pub async fn run() -> Result<JsValue, JsValue> {
     let mut opts = RequestInit::new();
@@ -49,6 +67,32 @@ pub async fn run() -> Result<JsValue, JsValue> {
     opts.mode(RequestMode::Cors);
 
     let url = "https://dog.ceo/api/breeds/image/random";
+
+    let request = Request::new_with_str_and_init(url, &opts)?;
+
+    let window = web_sys::window().unwrap();
+    let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
+
+    // `resp_value` is a `Response` object.
+    assert!(resp_value.is_instance_of::<Response>());
+    let resp: Response = resp_value.dyn_into().unwrap();
+
+    // Convert this other `Promise` into a rust `Future`.
+    let json = JsFuture::from(resp.json()?).await?;
+
+    // Use serde to parse the JSON into a struct.
+    let branch_info: Dog = json.into_serde().unwrap();
+
+    Ok(JsValue::from_serde(&branch_info).unwrap())
+}
+
+#[wasm_bindgen]
+pub async fn get_psql_data() -> Result<JsValue, JsValue> {
+    let mut opts = RequestInit::new();
+    opts.method("GET");
+    opts.mode(RequestMode::Cors);
+
+    let url = "http://localhost:3000/posts";
 
     let request = Request::new_with_str_and_init(url, &opts)?;
 
--- www/src/App.tsx
+++ www/src/App.tsx
@@ -1,24 +1,36 @@
 import React from "react";
 import * as wasm from "record-memo";
-// import { useEffect, useState } from 'react';
-
-var testarr = new Array();
-async function test(): Promise<String> {
-  var test2 = await wasm.run();
-  console.log(test2.message);
-  const str = test2.message;
-  testarr.push(str);
-  return str;
-}
-const t = test().then(() => console.log(testarr));
-const t2 = test().then(() => { return testarr[0] });
 
 const App = () => {
-  const test = wasm.greet();
-  console.log(t2)
+  type Dog = {
+    status: string;
+    message: string;
+  };
+  var arr: Array<Dog> = [];
+  function req_dog(): Promise<Dog> {
+    return wasm.run().then((data: Dog) => {
+      arr.push(data);
+      return data;
+    })
+  }
+  async function get_dog_data() {
+    const result = await req_dog();
+    return result;
+  }
+
+  get_dog_data().then((d) => {
+    localStorage.setItem('imageUrl', d.message);
+  })
+
+  const dogImgUrl = localStorage.imageUrl;
+
+  function DogImg() {
+    return <img src={dogImgUrl}></img>
+  }
   return (
     <div>
       <div>wasm-react-test</div>
+      <DogImg></DogImg>
     </div>
   );
 };
