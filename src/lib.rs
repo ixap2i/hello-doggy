92c8d1b3fbeedadf6e40d177092e47c81bb3f188
ixap2i <ayamashita124@gmail.com>
2023-01-03 23:56:42 +0900

    remove unneccesary method

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,8 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 #[wasm_bindgen]
 extern {
     fn alert(s: &str);
+    #[wasm_bindgen(catch)]
+    async fn run() -> Result<JsValue, JsValue>;
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -69,26 +71,3 @@ pub async fn run() -> Result<JsValue, JsValue> {
     Ok(serde_wasm_bindgen::to_value(&dog_image)?)
 }
 
-#[wasm_bindgen]
-pub async fn get_a_dog_data() -> Result<JsValue, JsValue> {
-    let mut opts = RequestInit::new();
-    opts.method("GET");
-    opts.mode(RequestMode::Cors);
-
-    let url = "http://localhost:3000/posts";
-
-    let request = Request::new_with_str_and_init(url, &opts)?;
-
-    let window = web_sys::window().unwrap();
-    let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
-
-    // `resp_value` is a `Response` object.
-    assert!(resp_value.is_instance_of::<Response>());
-    let resp: Response = resp_value.dyn_into().unwrap();
-
-    // Convert this other `Promise` into a rust `Future`.
-    let json = JsFuture::from(resp.json()?).await?;
-
-    let dog_image: Dog = serde_wasm_bindgen::from_value(json)?;
-    Ok(serde_wasm_bindgen::to_value(&dog_image)?)
-}
